d3dd275a3b93b95914571e1bf57ad8e9cdfb7377
dikkini <dikkini@gmail.com>
2013-06-04 21:16:51 +0400

    экран MyFriends

--- src/com/lookmyway/activity/common/Constants.java
+++ src/com/lookmyway/activity/common/Constants.java
@@ -4,4 +4,7 @@ package com.lookmyway.activity.common;
  * Created by haribo on 03.06.13.
  */
 public class Constants {
+    public static final int MY_FRIENDS_FRAGMENT_PAGE = 1;
+    public static final int MY_INFO_FRAGMENT_PAGE = 0;
+    public static final int MY_WAY_FRAGMENT_PAGE = 2;
 }
